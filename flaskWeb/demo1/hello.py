37902d464f01baf45cad309fe33413d8fe7b348c
kk <1181946155@qq.com>
2017-09-28 18:01:44 +0800

    app 处理返回中文报错

--- flaskWeb/demo1/hello.py
+++ flaskWeb/demo1/hello.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
+#coding=utf-8
 
 from flask import Flask
+from flask import request
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,10 +15,18 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
-
+# 路径参数
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello, %s!</h1>' %name
+
+# 获取浏览器代理
+@app.route('/browser')
+def browser():
+    user_agent = request.headers.get('User-Agent')
+    print user_agent
+    return '<p>您的浏览器是: %s</p>'%user_agent
+    # return '<p>your browser is: %s</p>'%user_agent
 
 if __name__ == '__main__':
     app.run(debug=True)
